fb22b669e2ae231585a859cc5b5bab03ac5ac3b4
paloma810 <mcyk49129@gmail.com>
2023-10-14 23:17:19 +0900

    fix CIDR of subnet for GKE

--- environments/dev/k8s/main.tf
+++ environments/dev/k8s/main.tf
@@ -10,7 +10,7 @@ module "gke" {
   // 最小範囲ののCIDRが変わるので注意
   // https://cloud.google.com/kubernetes-engine/docs/concepts/alias-ips?hl=ja#range_management
 
-  gke_master_ip_cidr   = "192.168.100.0/28"
+  gke_master_ip_cidr   = "192.168.100.0/24"
   gke_pods_ip_cidr     = "172.18.0.0/18"
   gke_services_ip_cidr = "172.19.0.0/18"
 
