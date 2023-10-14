23efdaea8e79603ded20174f0035bea3a34f0570
paloma810 <mcyk49129@gmail.com>
2023-10-14 23:57:41 +0900

    change GKE cluster IP CIDR

--- environments/dev/k8s/main.tf
+++ environments/dev/k8s/main.tf
@@ -10,8 +10,8 @@ module "gke" {
   // 最小範囲ののCIDRが変わるので注意
   // https://cloud.google.com/kubernetes-engine/docs/concepts/alias-ips?hl=ja#range_management
 
-  gke_master_ip_cidr   = "192.168.100.0/24"
-  gke_pods_ip_cidr     = "172.10.0.0/16"
-  gke_services_ip_cidr = "172.20.0.0/16"
+  gke_master_ip_cidr   = "172.10.0.0/16"
+  gke_pods_ip_cidr     = "172.20.0.0/16"
+  gke_services_ip_cidr = "172.30.0.0/16"
 
 }
