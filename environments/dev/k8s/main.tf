c80f52cd5b183b661cf5a873091548ae07cc2622
paloma810 <mcyk49129@gmail.com>
2023-10-16 22:23:06 +0900

    some change due to adding cicd functions for gke.

--- environments/dev/k8s/main.tf
+++ environments/dev/k8s/main.tf
@@ -15,3 +15,74 @@ module "gke" {
   gke_services_ip_cidr = "192.168.102.0/24"
 
 }
+
+# GKE サービスアカウントへの権限付与
+resource "google_project_iam_member" "sa_gke_cluster_iam_policy" {
+  project = var.cicd_project_id
+  role    = "roles/artifactregistry.reader" # 最小権限の法則のもと、一旦Artifact Registry の読み取り権限のみ付与
+  member  = module.gke.sa_gke_cluster_email
+}
+
+# gke用 Artifact Registryリポジトリ
+resource "google_artifact_registry_repository" "image_repo" {
+  project       = var.cicd_project_id
+  location      = "asia-northeast1"
+  repository_id = format("%s-%s", var.cicd_project_name, "repo-gke")
+  description   = "Docker repository for GKE application images"
+  format        = "DOCKER"
+}
+
+data "google_service_account" "sa_build" {
+  project    = var.cicd_project_id
+  account_id = "181997179469@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_artifact_registry_repository_iam_member" "terraform-image-iam" {
+  project    = var.cicd_project_id
+  location   = google_artifact_registry_repository.image_repo.location
+  repository = google_artifact_registry_repository.image_repo.name
+  role       = "roles/artifactregistry.admin"
+  member     = "serviceAccount:${data.google_service_account.sa_build.email}"
+}
+
+# Cloud Build
+resource "google_cloudbuild_trigger" "gke_app_build_trigger" {
+  project  = var.cicd_project_id
+  name     = "${var.cicd_project_name}-trigger-k8s"
+  location = "global"
+  github {
+    owner = "paoma810"
+    name  = "paloma-cicd-gitops-k8s"
+    push {
+      branch       = ".*"
+      invert_regex = false
+    }
+  }
+
+  filename = ""
+}
+locals {
+  // ref) default value of google cloud module "secure-ci"
+  roles_sa_build = [
+    "roles/artifactregistry.admin",
+    "roles/binaryauthorization.attestorsVerifier",
+    "roles/cloudbuild.builds.builder",
+    "roles/clouddeploy.developer",
+    "roles/clouddeploy.releaser",
+    "roles/cloudkms.cryptoOperator",
+    "roles/containeranalysis.notes.attacher",
+    "roles/containeranalysis.notes.occurrences.viewer",
+    "roles/source.writer",
+    "roles/storage.admin",
+    "roles/cloudbuild.workerPoolUser",
+    "roles/ondemandscanning.admin",
+    "roles/logging.logWriter"
+  ]
+}
+
+resource "google_project_iam_member" "sa_build_project_iam" {
+  for_each = toset(local.roles_sa_build)
+  project  = var.cicd_project_id
+  role     = each.value
+  member   = data.google_service_account.sa_build.email
+}
--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -2,3 +2,8 @@ output "gke_cluster_name" {
   description = "GKE cluster name"
   value       = google_container_cluster.gke_cluster.name
 }
+
+output "sa_gke_cluster_email" {
+  description = "Service Account using by GKE Cluster"
+  value       = google_service_account.sa_gke_cluster.email
+}
