b1c4f7803c6012301682ce803fa98892ef14a38e
paloma810 <mcyk49129@gmail.com>
2025-02-15 20:20:49 +0900

    change all tf file

--- environments/dev/common/variables.tf
+++ environments/dev/common/variables.tf
@@ -21,13 +21,18 @@ variable "is_create_aws_instance" {
   type    = number
   default = 0
 }
-variable "is_create_vpn_with_aws" {
+variable "is_create_vpn" {
+  type    = number
+  default = 0
+}
+variable "is_create_aws_resources" {
   type    = number
   default = 0
 }
 
 /* AWS用変数 */
-/* AWS用変数 */
+// アクセスキーとシークレットキーの値を入れた状態だとgithubにアップロードできない
+// 故にAWSリソースはCICD対象とはせず、ローカルで値を補完した上でterraformコマンドでデプロイする
 variable "aws_access_key" {
   type    = string
   default = "NONE"
