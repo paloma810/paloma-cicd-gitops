8c630c5337067099eb96196f5a5a4ebb069f37ed
paloma810 <mcyk49129@gmail.com>
2025-04-13 10:18:49 +0900

    fix aws components and variables

--- components/aws/common/provider.tf
+++ components/aws/common/provider.tf
@@ -12,24 +12,6 @@ terraform {
   required_version = ">= 1.3.0"
 }
 
-/* Googleの認証はサービスアカウントキーではなく
-   gcloud auth application-default loginで実施する */
-provider "google" {
-  #credentials   = file(var.gcp_credential_filename)
-  project = var.gcp_project_hub
-  region  = "asia-northeast1"
-  zone    = "asia-northeast1-a"
-}
-
-provider "google" {
-  alias = "spoke"
-
-  #credentials   = file(var.gcp_credential_filename)
-  project = var.gcp_project_spoke
-  region  = "asia-northeast1"
-  zone    = "asia-northeast1-a"
-}
-
 /* AWSの認証はIAMクレデンシャル */
 provider "aws" {
   access_key = var.aws_access_key
--- components/aws/common/variables.tf
+++ components/aws/common/variables.tf
@@ -1,18 +1,4 @@
 /* GCP用変数 */
-/*
-variable "gcp_credential_filename" {
-  type    = string
-  default = "./gcp_credentioal.json"
-}
-*/
-variable "gcp_project_hub" {
-  type    = string
-  default = "kh-paloma-m01-01"
-}
-variable "gcp_project_spoke" {
-  type    = string
-  default = "kh-paloma-m01-02"
-}
 variable "is_create_aws_instance" {
   type    = number
   default = 0
